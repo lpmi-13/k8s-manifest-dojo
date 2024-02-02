a0610e4282181fe0c366e96782baabe9178b73bf
Adam Leskis <leskis@gmail.com>
2024-02-02 11:05:46 +0000

    add in some data and volume mounts to expand on later

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,17 @@
+if ! [ -d "/tmp/k3s-data" ]; then
+  mkdir /tmp/k3s-data
+fi
+
+for i in {1..100}; do
+  echo "this is log message #${i}" > /tmp/k3s-data/${i}.txt
+done
+
+# we need to set up the data and the persistent volumes first
+kubectl apply -f manifests/webserver-pv.yaml
+
+kubectl apply -f manifests/webserver-pvc.yaml
+
+# and once the storage is set up, we can actually deploy the pods
 kubectl apply -f manifests/webserver-deployment.yaml
 
 kubectl apply -f manifests/webserver-service.yaml
--- webserver/app.py
+++ webserver/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def check_datetime():
+    with open("/tmp/k3s-data/1.txt", "r") as log_file:
+        log_data = log_file.readlines()
+
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return f'Current date and time is: {current_time}'
+    return f'Current date and time is: {current_time} and also {log_data[0]}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
