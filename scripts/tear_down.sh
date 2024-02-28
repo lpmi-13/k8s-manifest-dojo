2176722dbdb5d7595b10daeadec4e578bbe6119a
Adam Leskis <leskis@gmail.com>
2024-02-28 23:43:38 +0000

    fix order of deletion in tear down script

--- scripts/tear_down.sh
+++ scripts/tear_down.sh
@@ -1,3 +1,3 @@
-for resource in deploy svc pv pvc sts secrets configmap roles rolebinding sa networkpolicy; do
+for resource in deploy svc sts secrets configmap roles rolebinding sa networkpolicy pvc pv; do
   kubectl delete $resource -n dojo --all;
 done
