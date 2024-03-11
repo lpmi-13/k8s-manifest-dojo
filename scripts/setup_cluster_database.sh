80ceb462396cde6ccac1945a8168f4150bae9e9a
Adam Leskis <leskis@gmail.com>
2024-03-11 23:31:52 +0000

    pass namespace explicitly

--- scripts/setup_cluster_database.sh
+++ scripts/setup_cluster_database.sh
@@ -1,3 +1,3 @@
-DATABASE_ENDPOINT=$(kubectl get po postgres-0 -o=jsonpath='{.status.podIP}')
+DATABASE_ENDPOINT=$(kubectl -n dojo get po postgres-0 -o=jsonpath='{.status.podIP}')
 
 PGPASSWORD=supersecretproductionpassword psql -h $DATABASE_ENDPOINT -U prod-user -f ./scripts/setup_users.sql
