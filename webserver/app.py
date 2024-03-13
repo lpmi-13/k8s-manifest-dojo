1372b4ce90ba591f7e10a39ea6621a0b24e57ab3
Adam Leskis <leskis@gmail.com>
2024-03-13 14:08:45 +0000

    connect webserver to downstream services

--- webserver/app.py
+++ webserver/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 import os
 import requests
 
-# we can just use the name of the service, due to DNS record creation on service creation
+# we can just use the name of the services, due to DNS record creation on service creation
 LOG_PROCESSOR_DNS_NAME = "logs-processor"
+USER_INFO_DNS_NAME = "user-info"
 ENVIRONMENT = os.getenv("ENVIRONMENT", default="DEVELOPMENT")
 
 
@@ -11,6 +12,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def default_path():
+    return "ready to serve content!"
+
+
+@app.route("/log-info")
 def log_info():
 
     try:
@@ -19,6 +25,28 @@ def log_info():
         print(f"received a total log count of: {log_count}")
 
         return f"environment is: {ENVIRONMENT}, and we have {log_count} total log lines"
+    except Exception as e:
+        print(f"received an error: {e}")
+
+
+@app.route("/all_users")
+def all_users():
+    try:
+        response = requests.get(f"http://{USER_INFO_DNS_NAME}/users")
+        users = response.json()
+
+        return {"all_users": users}
+    except Exception as e:
+        print(f"received an error: {e}")
+
+
+@app.route("/user/<int:user_id>")
+def get_user_by_id(user_id):
+    try:
+        response = requests.get(f"http://{USER_INFO_DNS_NAME}/user/{user_id}")
+        user = response.json()
+
+        return {"user": user}
     except Exception as e:
         print(f"received an error: {e}")
 
