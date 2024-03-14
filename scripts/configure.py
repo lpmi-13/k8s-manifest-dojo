b44683066329aef52ed7f1b9c98f28b677e3886a
Adam Leskis <leskis@gmail.com>
2024-03-14 16:13:48 +0000

    allow multiple replacements per file

--- scripts/configure.py
+++ scripts/configure.py
@@ -5,42 +5,52 @@ import os
 DIRECTORY_PATH = "./manifests/application/"
 
 CONFIGURATION = {
-    "database-service.yaml": {
-        "replace": "DATABASE_PORT",
-        "good": "5432",
-        "bad": "4321",
-    },
+    "database-service.yaml": [
+        {
+            "replace": "DATABASE_PORT",
+            "good": "5432",
+            "bad": "4321",
+        }
+    ],
     # comment this out for now because the behavior seems a bit flaky and non-deterministic
     # "logs-processor-deployment.yaml": {
     # "replace": "DELAY_IN_SECONDS",
     # "good": "30",
     # "bad": "1",
     # },
-    "network-policy-database.yaml": {
-        "replace": "INGRESS_FROM_POD",
-        "good": "user-info",
-        "bad": "user-service",
-    },
-    "user-info.deployment.yaml": {
-        "replace": "MEMORY_LIMIT",
-        "good": "512Mi",
-        "bad": "1Mi",
-    },
-    "user-info.deployment.yaml": {
-        "replace": "CPU_LIMIT",
-        "good": "500m",
-        "bad": "1m",
-    },
-    "webserver-deployment.yaml": {
-        "replace": "SERVICE_ACCOUNT_NAME",
-        "good": "flask-webserver-service-account",
-        "bad": "flask-webserver-service-account-no-access",
-    },
-    "webserver-serviceaccount.yaml": {
-        "replace": "SECRET_NAME",
-        "good": "db-credentials",
-        "bad": "secret-credentials",
-    },
+    "network-policy-database.yaml": [
+        {
+            "replace": "INGRESS_FROM_POD",
+            "good": "user-info",
+            "bad": "user-service",
+        }
+    ],
+    "user-info-deployment.yaml": [
+        {
+            "replace": "MEMORY_LIMIT",
+            "good": "512Mi",
+            "bad": "1Mi",
+        },
+        {
+            "replace": "CPU_LIMIT",
+            "good": "500m",
+            "bad": "1m",
+        },
+    ],
+    "webserver-deployment.yaml": [
+        {
+            "replace": "SERVICE_ACCOUNT_NAME",
+            "good": "flask-webserver-service-account",
+            "bad": "flask-webserver-service-account-no-access",
+        }
+    ],
+    "webserver-serviceaccount.yaml": [
+        {
+            "replace": "SECRET_NAME",
+            "good": "db-credentials",
+            "bad": "secret-credentials",
+        }
+    ],
 }
 
 FILES_TO_CONFIGURE = [file_name for file_name in CONFIGURATION]
@@ -51,18 +61,25 @@ def choose_random(item_list):
     return item_list[random.randint(0, len(item_list) - 1)]
 
 
-def replace_line_in_file(file_name, word_to_replace, replacement):
-    contents = ""
-    with open(os.path.join(DIRECTORY_PATH, file_name), "r") as original_file:
-        for line in original_file:
-            temp_contents = line.rstrip().replace(
-                word_to_replace,
-                replacement,
-            )
-            contents += temp_contents + "\n"
+def replace_line_in_file(file_name, misconfigure=False):
+    for change in CONFIGURATION[file_name]:
+        contents = ""
+        word_to_replace = change["replace"]
+        if misconfigure:
+            replacement = change["bad"]
+        else:
+            replacement = change["good"]
 
-    with open(os.path.join(DIRECTORY_PATH, file_name), "w") as updated_file:
-        updated_file.write(contents)
+        with open(os.path.join(DIRECTORY_PATH, file_name), "r") as original_file:
+            for line in original_file:
+                temp_contents = line.rstrip().replace(
+                    word_to_replace,
+                    replacement,
+                )
+                contents += temp_contents + "\n"
+
+        with open(os.path.join(DIRECTORY_PATH, file_name), "w") as updated_file:
+            updated_file.write(contents)
 
 
 FILE_TO_MISCONFIGURE = choose_random(FILES_TO_CONFIGURE)
@@ -72,13 +89,7 @@ for file_name in FILES_TO_CONFIGURE:
     if file_name != FILE_TO_MISCONFIGURE:
         replace_line_in_file(
             file_name,
-            CONFIGURATION[file_name]["replace"],
-            CONFIGURATION[file_name]["good"],
         )
 
 # now we update one value to break things
-replace_line_in_file(
-    FILE_TO_MISCONFIGURE,
-    CONFIGURATION[FILE_TO_MISCONFIGURE]["replace"],
-    CONFIGURATION[FILE_TO_MISCONFIGURE]["bad"],
-)
+replace_line_in_file(FILE_TO_MISCONFIGURE, misconfigure=True)
