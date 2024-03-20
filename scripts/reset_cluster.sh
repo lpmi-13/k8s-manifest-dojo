695d8ded9be5f510a4b2456fb98b60b131dc7f57
Adam Leskis <leskis@gmail.com>
2024-03-20 23:46:15 +0000

    rework directories so the broken config is less obvious

--- scripts/reset_cluster.sh
+++ scripts/reset_cluster.sh
@@ -1,5 +1,12 @@
 # First, tear down anything currently running
 ./scripts/tear_down.sh
 
+# Clear out current application manifests and copy the templates back in
+rm -rf "./manifests/application/*"
+cp -r "./manifests/templates" "./manifests/application"
+
+# set up the manifests with one broken configuration
+python3 ./scripts/configure.py
+
 # And now reset the deployments with corresponding services
 ./scripts/deploy.sh
