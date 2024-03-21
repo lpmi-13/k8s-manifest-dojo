8cd4582c5d55ea5f5c0bdef16bd21fe71998cbeb
Adam Leskis <leskis@gmail.com>
2024-03-21 11:08:01 +0000

    move all the app code into a separate folder

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,15 +1,15 @@
-docker build -t flask-webserver -f webserver/Dockerfile webserver/
+docker build -t flask-webserver -f service-src/webserver/Dockerfile service-src/webserver/
 docker tag flask-webserver localhost:5001/flask-webserver
 docker push localhost:5001/flask-webserver
 
-docker build -t logs-processor -f logs-processor/Dockerfile logs-processor/
+docker build -t logs-processor -f service-src/logs-processor/Dockerfile service-src/logs-processor/
 docker tag logs-processor localhost:5001/logs-processor
 docker push localhost:5001/logs-processor
 
-docker build -t user-info -f user-info/Dockerfile user-info/
+docker build -t user-info -f service-src/user-info/Dockerfile service-src/user-info/
 docker tag user-info localhost:5001/user-info
 docker push localhost:5001/user-info
 
-docker build -t load-generator -f load-generator/Dockerfile load-generator/
+docker build -t load-generator -f service-src/load-generator/Dockerfile service-src/load-generator/
 docker tag load-generator localhost:5001/load-generator
 docker push localhost:5001/load-generator
