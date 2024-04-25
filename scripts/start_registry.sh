3eec160590976a42b22f5837c4ff2739afa08c24
Adam Leskis <leskis@gmail.com>
2024-04-25 14:50:35 +0100

    keep simplifying script

--- scripts/start_registry.sh
+++ scripts/start_registry.sh
@@ -1,6 +1,6 @@
-docker stop private-registry
-
-docker rm private-registry
+if [ -n "$(docker ps -f "name=private-registry" -f "status=running" -q )" ]; then
+    docker stop private-registry && docker rm private-registry
+fi
 
 # we're binding this to port 5001 just in case we want something else to be able to use the standard 5000 port
 docker run -d -p 5001:5000 --restart always --name private-registry registry:latest
